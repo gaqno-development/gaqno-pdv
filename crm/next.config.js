1cf2280ef9f6c43fa5d9b046912fb84b8dabb20d
Gabriel Aquino <gabriel.deaquino-ext@ab-inbev.com>
2025-12-09 21:17:36 -0300

    Add Dockerfiles and enable standalone output

--- crm/next.config.js
+++ crm/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // Enable standalone output for Docker
   transpilePackages: ["@repo/ui", "@repo/core"],
   assetPrefix: "/crm",
 }
--- platform/next.config.js
+++ platform/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // Enable standalone output for Docker
   transpilePackages: ["@repo/ui", "@repo/core"],
   assetPrefix: "/dashboard", // For static assets
 }
